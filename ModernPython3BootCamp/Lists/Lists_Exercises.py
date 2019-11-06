cb814983cd64071358cb357f4d384f8fa1beaf69
Flo Og <tuffguy_20708@yahoo.com>
2019-11-06 05:11:22 -0500

    20191106 Commit

---
+++ ModernPython3BootCamp/Lists/Lists_Exercises.py
@@ -0,0 +1,34 @@
+# nums = list(range(1,100))
+#
+# for num in nums:
+#     print(num)
+
+# sounds = ["super", "cali", "frali"]
+# result = ''
+# for sound in sounds:
+#     result += sound
+# print(result.upper())
+
+# Create a list called instructors
+instructors = []
+# Add the following strings to the instructors list
+# "Colt"
+# "Blue"
+# "Lisa"
+# instructors.append("Colt")
+# instructors.append("Blue")
+# instructors.append("Lisa")
+#
+# # Remove the last value in the list
+# instructors.pop()
+# # Remove the first value in the list
+# del instructors[0]
+# # Add the string "Done" to the beginning of the list
+# instructors.insert(0,"Done")
+# # Run the tests to make sure you've done this correctly!
+# print(instructors)
+#
+# mountains = "/\\/\\/\\"
+# print(mountains)
+quotation = "\"\""
+print(quotation)
