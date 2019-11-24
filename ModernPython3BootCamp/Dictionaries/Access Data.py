c5170f06f8c2f2ced1c40cc2031e148899b68881
Flo Og <tuffguy_20708@yahoo.com>
2019-11-24 16:28:48 -0500

    20191106 Commit

---
+++ ModernPython3BootCamp/Dictionaries/Access Data.py
@@ -0,0 +1,13 @@
+# Given the dictionary below:
+#
+# artist = {
+#     "first": "Neil",
+#     "last": "Young",
+# }
+
+artist = {
+    "first": "Neil",
+    "last": "Young",
+}
+
+print(artist["first"])
