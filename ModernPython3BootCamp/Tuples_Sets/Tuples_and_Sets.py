d1f6b38153ae8a178aec3df159315a4ed254933a
Funlola Ogunleye <fogunleye@leaseaccelerator.com>
2019-11-27 18:34:38 -0500

    Tuples and Sets 20191127

---
+++ ModernPython3BootCamp/Tuples_Sets/Tuples_and_Sets.py
@@ -0,0 +1,18 @@
+# 1 - Create a variable called numbers which is a tuple with the the values 1, 2, 3 and 4 inside.
+
+
+# 2 - Create a variable called value which is a tuple with the the value 1 inside.
+
+
+# 3 - Given the following variable:
+
+values = [10,20,30]
+
+# Create a variable called static_values which is the result of the values variable converted to a tuple
+
+
+# 4 - Given the following variable
+
+stuff = [1,3,1,5,2,5,1,2,5]
+
+# Create a variable called unique_stuff which is a set of only the unique values in the stuff list
