0ab8449b2de12a2caf3e8762aa6e377e52e1b728
Flo Og <tuffguy_20708@yahoo.com>
2020-03-20 15:54:51 -0400

    Initial Commit

---
+++ LA-Python_Intro/Deployment/notes/env.bat
@@ -0,0 +1,7 @@
+REM Windows equivalent of sourcing the batch file
+set DB_USERNAME='flopg'
+set DB_PASSWORD='flopgpw1'
+set DB_HOST='flo2c.mylabserver.com'
+set DB_PORT='80'
+set FLASK_ENV='development'
+set FLASK_APP='.'
