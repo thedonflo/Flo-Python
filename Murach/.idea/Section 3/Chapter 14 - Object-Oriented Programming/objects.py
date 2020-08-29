6262e9c07357255d68574b17c82af5fc99bc3135
thedonflo <54419917+thedonflo@users.noreply.github.com>
2020-08-29 16:17:45 -0400

    Delete objects.py

--- Murach/.idea/Section 3/Chapter 14 - Object-Oriented Programming/objects.py
+++
@@ -1,7 +0,0 @@
-class Movie:
-    def __init__(self, name, year):
-        self.name = name
-        self.year = year
-
-    def getStr(self):
-        return self.name + " ("  + str(self.year) + ")"
